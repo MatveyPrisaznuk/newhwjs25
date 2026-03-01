24a70eda71b6db3ae6dc5d66783837fabfe97306
Matvey <matveyprys@gmail.com>
2026-03-01 11:34:13 +0200

    Updates

---
+++ newhwjs25.4394d1a6.js
@@ -0,0 +1,5 @@
+let e=document.querySelector("#bookmarkInput"),t=document.querySelector("#bookmarkList"),a=document.querySelector("#addBookmarkBtn"),l=JSON.parse(localStorage.getItem("bookmarks"));function o(){let e=l.map((e,t)=>`<li class="list__item">
+        <a href="${e}" class="link">${e}</a>
+        <button id="addBookmarkBtn" data-action="${t}">X</button>
+    </li>`).join("");localStorage.setItem("bookmarks",JSON.stringify(l)),t.innerHTML=e}o(),a.addEventListener("click",()=>{let t=e.value.trim();t&&(l.push(t),e.value="",o())}),t.addEventListener("click",e=>{let t=e.target.nodeName,a=e.target.dataset.action;"BUTTON"===t&&(l.splice(a,1),o())});let n=document.querySelector("#saveBtn"),r=document.querySelector(".form").elements;n.addEventListener("click",e=>{localStorage.clear(),e.preventDefault()});for(let e=0;e<r.length;e+=1)r[e].addEventListener("change",c);function c(){localStorage.setItem(this.name,this.value)}for(let e=0;e<r.length;e+=1)if("submit"!==r[e].type){let t=localStorage.getItem(r[e].name);null!==t&&(r[e].value=t)}
+//# sourceMappingURL=newhwjs25.4394d1a6.js.map
